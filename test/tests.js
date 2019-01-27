ddf8e0f5e7fff8946254fe6840cd602a5521df11
JackTorrence <jacktorrence420@gmx.com>
2019-01-27 17:02:39 +0000

    add come chai

---
+++ test/tests.js
@@ -0,0 +1,99 @@
+var expect = require('chai').expect;
+
+
+
+describe('isValid()', function () {
+    it('should return true with a valid tag', function () {
+      
+      // 1. ARRANGE
+      var ShutdownTag = require('../shutdownTag.js');
+      var tag = 'GMT-14'
+      var shutdownTag = new ShutdownTag(tag);
+      // 2. ACT
+      var result = shutdownTag.isValid()
+  
+      // 3. ASSERT
+      expect(result).to.be.true;
+  
+    })
+});
+
+describe('isValid()', function () {
+    it('should return false with a invalid tag', function () {
+      
+      // 1. ARRANGE
+      var ShutdownTag = require('../shutdownTag.js');
+      var tag = 'GMT-79'
+      var shutdownTag = new ShutdownTag(tag);
+      // 2. ACT
+      var result = shutdownTag.isValid()
+  
+      // 3. ASSERT
+      expect(result).to.be.false;
+  
+    })
+});
+
+describe('isValidTagHour()', function () {
+    it('should return true with a valid hour', function () {
+      
+      // 1. ARRANGE
+      var ShutdownTag = require('../shutdownTag.js');
+      var tag = 'GMT-24'
+      var shutdownTag = new ShutdownTag(tag);
+      // 2. ACT
+      var result = shutdownTag.isValidHour(shutdownTag.splitShutDownTagParts(tag)[1])
+  
+      // 3. ASSERT
+      expect(result).to.be.true;
+  
+    })
+});
+
+describe('isValidTagHour()', function () {
+    it('should return false with an invalid hour', function () {
+      
+      // 1. ARRANGE
+      var ShutdownTag = require('../shutdownTag.js');
+      var tag = 'GMT-25'
+      var shutdownTag = new ShutdownTag(tag);
+      // 2. ACT
+      var result = shutdownTag.isValidHour(shutdownTag.splitShutDownTagParts(tag)[1])
+  
+      // 3. ASSERT
+      expect(result).to.be.false;
+  
+    })
+});
+
+describe('isValidZone()', function () {
+    it('should return false with an invalid tag', function () {
+      
+      // 1. ARRANGE
+      var ShutdownTag = require('../shutdownTag.js');
+      var tag = 'PPP-25'
+      var shutdownTag = new ShutdownTag(tag);
+      // 2. ACT
+      var result = shutdownTag.isValidZone(shutdownTag.splitShutDownTagParts(tag)[0])
+  
+      // 3. ASSERT
+      expect(result).to.be.false;
+  
+    })
+});
+
+describe('isValidZone()', function () {
+    it('should return true with an valid tag', function () {
+      
+      // 1. ARRANGE
+      var ShutdownTag = require('../shutdownTag.js');
+      var tag = 'GMT-24'
+      var shutdownTag = new ShutdownTag(tag);
+      // 2. ACT
+      var result = shutdownTag.isValidZone(shutdownTag.splitShutDownTagParts(tag)[0])
+  
+      // 3. ASSERT
+      expect(result).to.be.true;
+  
+    })
+});
